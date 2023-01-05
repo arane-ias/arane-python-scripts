5ae87a517633a2ba53fcdf1476b4bfc06fd5ab20
Aniket Rane <aniketrane0557@gmail.com>
2023-01-05 21:45:24 +0530

    deleted files

--- collect snowflake partner data/test02.py
+++
@@ -1,7 +0,0 @@
-import re
-
-s3_path = "aws-athena-query-results-972380794107-us-east-1/arane/000b5061-bd3e-45e8-bc50-a98695a8bdef.csv"
-
-filename = re.findall('.*\/(.*)', s3_path)[0]
-
-print(filename)
